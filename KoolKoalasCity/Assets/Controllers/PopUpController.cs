157fb6e667b9d603c5a6ab277fb87366c7d25e8e
algrau <algraud@gmail.com>
2019-02-28 08:46:45 +0200

    created gameobjects

---
+++ KoolKoalasCity/Assets/Controllers/PopUpController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpController : MonoBehaviour {
+
+    bool QuestionAnswered;
+    Question[] Questions;
+	// Use this for initialization
+	void Start () {
+        QuestionAnswered = false;
+        GetQuestion();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GetQuestion()
+    {
+        //retrieve random questions from file
+        if (Questions.Length == 0)
+        {
+            //create sample question
+        }
+    }
+
+    public class Question
+    {
+        public string QuestionString { get; private set; }
+        public string[] Options { get; private set; }
+
+        public Question(string question, string correctOption, string option2, string option3, string option4)
+        {
+            //fill
+        }
+
+    }
+}
