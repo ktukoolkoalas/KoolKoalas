b4deda6f52a5634a8b62f083aa27133cac68fead
gabsub <subaciutegabija9@gmail.com>
2019-03-05 17:19:16 +0200

    Miesto vaizdas

---
+++ KoolKoalasCity/Assets/scripts/BackButtonClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButtonClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ClickTheButton(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+}
---
+++ KoolKoalasCity/Assets/scripts/BuildingClicker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BuildingClicker : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        { 
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);
+                if (hit.transform.name == "Cube (1)" || hit.transform.name == "Cube")
+                {
+                    SceneManager.LoadScene("GameScene");
+                }
+            }          
+           
+
+        }
+    }
+}
