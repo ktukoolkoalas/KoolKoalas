d81243017b4128702d3020a64683dbb1382cdb74
GretaKr <krylovaite@gmail.com>
2019-03-05 18:53:25 +0200

    KoinStart

---
+++ KoolKoalasCity/Assets/scripts probably/KoalaKoins.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KoalaKoins : MonoBehaviour
+{
+
+    public Text koinText;
+    private int koinCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        koinCounter = 1000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        subtractKoin(50);
+  
+        koinText.text = koinCounter.ToString();
+        
+    }
+
+    int addKoin (int koinsToAdd)
+    {
+        koinCounter += koinsToAdd;
+
+        return koinCounter;
+    }
+
+    bool subtractKoin(int koinsToSubtract)
+    {
+        if (koinsToSubtract <= koinCounter)
+        {
+            koinCounter -= koinsToSubtract;
+
+            return true;
+        }
+
+        return false;
+    }
+}
