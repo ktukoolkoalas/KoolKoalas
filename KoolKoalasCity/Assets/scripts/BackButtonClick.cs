d0a0de6e4c2838d5cf4bc7863afc35a34848b21c
gabsub <subaciutegabija9@gmail.com>
2019-03-05 19:18:54 +0200

    miesto vaizdas pataisytas

--- KoolKoalasCity/Assets/scripts/BackButtonClick.cs
+++ KoolKoalasCity/Assets/scripts/BackButtonClick.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BackButtonClick : MonoBehaviour
 {
-    // Start is called before the first frame update
+        // Start is called before the first frame update
     public void ClickTheButton(string scenename)
     {
         Application.LoadLevel(scenename);
