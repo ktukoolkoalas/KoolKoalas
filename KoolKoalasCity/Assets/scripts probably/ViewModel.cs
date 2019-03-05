2b1edd85ea5a741908f51bee7a1dbc9111fc5f6d
Ieva-0 <lukoseviciute.ie@gmail.com>
2019-03-05 17:00:45 +0200

    :(

---
+++ KoolKoalasCity/Assets/scripts probably/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ViewModel : MonoBehaviour
+{
+    public Slider slider;
+
+    public void Button_Click()
+    {
+        StartCoroutine(FillBar());
+    }
+    IEnumerator FillBar()
+    {
+        float runtime = 1f;
+        for (int i = 0; i < 25; i++)
+        {
+            slider.value += (float)0.01;
+            yield return null;
+        }
+    }
+}
