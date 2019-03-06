7979d72c23334f01b3312b456e3f123d5b5c1998
algrau <algraud@gmail.com>
2019-03-06 09:24:52 +0200

    Merging with master

---
+++ KoolKoalasCity/Assets/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is the main script that will run the game.
+public class GameController : MonoBehaviour {
+
+    public GameObject PopupObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
