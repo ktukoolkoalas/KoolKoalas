bbbee0b974f6d9491fcfe8b020ffae755cf06b36
Ieva-0 <lukoseviciute.ie@gmail.com>
2019-03-09 15:11:02 +0200

    Created memory mini game

---
+++ KoolKoalasCity/Assets/Scripts/MemorySceneController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MemorySceneController : MonoBehaviour
+{
+    public const int gridRows = 2;
+    public const int gridCols = 4;
+    public const float offsetX = 4f;
+    public const float offsetY = 5f;
+    public int _matches = 0;
+
+    [SerializeField] private MainCard originalCard;
+    [SerializeField] private Sprite[] images;
+    private void Start()
+    {
+        Vector3 startPos = originalCard.transform.position;
+        int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3 };
+        numbers = ShuffleArray(numbers);
+
+        for(int i = 0; i < gridCols; i++)
+        {
+            for(int j = 0; j < gridRows; j++)
+            {
+                MainCard card;
+                if (i == 0 && j == 0)
+                {
+                    card = originalCard;
+                }
+                else
+                {
+                    card = Instantiate(originalCard) as MainCard;
+                }
+                int index = j * gridCols + i;
+                int id = numbers[index];
+                card.ChangeSprite(id, images[id]);
+
+                float posX = (offsetX * i) + startPos.x;
+                float posY = (offsetY * j) + startPos.y;
+                card.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+        }
+    }
+    private int[] ShuffleArray(int[] numbers)
+    {
+        int[] newArray = numbers.Clone() as int[];
+        for(int i = 0; i < newArray.Length; i++)
+        {
+            int tmp = newArray[i];
+            int r = Random.Range(i, newArray.Length);
+            newArray[i] = newArray[r];
+            newArray[r] = tmp;
+        }
+        return newArray;
+    }
+
+    private MainCard _firstRevealed;
+    private MainCard _secondRevealed;
+
+    private int _score = 0;
+    [SerializeField] private TextMesh scoreLabel;
+
+    public bool canReveal
+    {
+        get { return _secondRevealed == null; }
+    }
+
+    public void CardRevealed(MainCard card)
+    {
+        if(_firstRevealed == null)
+        {
+            _firstRevealed = card;
+        }
+        else
+        {
+            _secondRevealed = card;
+            StartCoroutine(CheckMatch());
+        } 
+    }
+    
+    private IEnumerator CheckMatch()
+    {
+        if(_firstRevealed.id == _secondRevealed.id)
+        {
+            _score++;
+            scoreLabel.text = "Score: " + _score;
+            _matches++;
+        }
+        else
+        {
+            _score--;
+            scoreLabel.text = "Score: " + _score;
+            yield return new WaitForSeconds(1f);
+            _firstRevealed.Unreveal();
+            _secondRevealed.Unreveal();
+        }
+        _firstRevealed = null;
+        _secondRevealed = null;
+        if(_matches == 4)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
