f328e21c6846710588f33d6f2f7dfec7acd8e9a0
MonikaLin <monika.linkaite@ktu.edu>
2019-03-20 00:09:18 +0200

    pataisytas miesto vaizdas

--- KoolKoalasCity/Assets/Controllers/BackgroundController.cs
+++ KoolKoalasCity/Assets/Controllers/BackgroundController.cs
@@ -33,4 +33,44 @@ public class BackgroundController : MonoBehaviour
         }
 
     }
+
+    //public DateTime currentTime;
+    //public DateTime unactivatedTimeCounter;
+
+    //public GameObject DayBackground;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    currentTime = DateTime.Now;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    bool activated = false;
+    //    for (int i = 0; i < 1440; i = i + 10)
+    //    {
+    //        activatedTimeCounter += DateTime.Now.AddMinutes(i);
+    //        print(activatedTimeCounter);
+    //    }
+
+    //    unactivatedTimeCounter += 20;
+    //    for (int i = currentTime.AddSeconds(10); i < currentTime.AddMinutes(1); i = currentTime.AddSeconds(activatedTimeCounter))
+    //    {
+    //        if (activated == false)
+    //        {
+    //            DayBackground.SetActive(true);
+    //            activated = false;                                             
+    //        }
+    //        for (int j = currentTime.AddSeconds(20); j < currentTime.AddMinutes(1); i = currentTime.AddSeconds(unactivatedTimeCounter))
+    //        {
+    //            if (activated == true)
+    //            {
+    //                DayBackground.SetActive(false);
+    //                activated = true;
+    //            }
+    //        }
+    //    }
+    //}
 }
