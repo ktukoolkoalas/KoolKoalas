3e0cfa4b8ef2374af1c659b727c5e1c4a3fd967c
MonikaLin <monika.linkaite@ktu.edu>
2019-04-01 22:48:17 +0300

    DemoShop

---
+++ KoolKoalasCity/Assets/Controllers/ShopController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopController : MonoBehaviour
+{
+    public GameObject ShopWindow;
+    public GameObject FoodStorage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowShop()
+    {
+        ShopWindow.SetActive(true);
+        ShopWindow.transform.SetAsLastSibling();
+    }
+
+    public void CloseShop()
+    {
+        ShopWindow.SetActive(false);
+        FoodStorage.SetActive(false);
+    }
+
+    public void ShowFoodStorage()
+    {
+        FoodStorage.SetActive(true);
+        FoodStorage.transform.SetAsLastSibling();
+    }
+
+}
