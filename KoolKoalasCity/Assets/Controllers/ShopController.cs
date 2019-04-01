5e2118d438841cd5785afc1b313bd0b7ea900086
MonikaLin <monika.linkaite@ktu.edu>
2019-04-02 00:39:38 +0300

    ShopDemo2

--- KoolKoalasCity/Assets/Controllers/ShopController.cs
+++ KoolKoalasCity/Assets/Controllers/ShopController.cs
@@ -7,36 +7,44 @@ public class ShopController : MonoBehaviour
 {
     public GameObject ShopWindow;
     public GameObject FoodStorage;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject StuffStorage;
+    public int FoodPrice = 5;
+    public Text Koins;
 
     public void ShowShop()
     {
         ShopWindow.SetActive(true);
         ShopWindow.transform.SetAsLastSibling();
+        Koins.text = GlobalData.KoinCounter.ToString();
     }
 
     public void CloseShop()
     {
         ShopWindow.SetActive(false);
         FoodStorage.SetActive(false);
+        StuffStorage.SetActive(false);
     }
 
     public void ShowFoodStorage()
     {
         FoodStorage.SetActive(true);
         FoodStorage.transform.SetAsLastSibling();
+    }
+
+    public void ShowStuffStorage()
+    {
+        StuffStorage.SetActive(true);
+        StuffStorage.transform.SetAsLastSibling();
+    }
+
+    public void BuyFood()
+    {
+        if (GlobalData.KoinCounter >= FoodPrice)
+        {
+            GlobalData.KoinChange = 5;
+            Koins.text = GlobalData.KoinCounter.ToString();
+            print(GlobalData.KoinCounter);
+        }        
     }
 
 }
