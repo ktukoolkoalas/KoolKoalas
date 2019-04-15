c83a0bd43abfef28fa826352cfda0bb601b8c4d3
MonikaLin <monika.linkaite@ktu.edu>
2019-04-15 22:48:55 +0300

    Siuksliu-rinkimas/Gabija

--- KoolKoalasCity/Assets/Controllers/ReserveController.cs
+++ KoolKoalasCity/Assets/Controllers/ReserveController.cs
@@ -5,6 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class ReserveController : MonoBehaviour
 {
+    public int TrashPrice = 10;
+    public GameObject trash1;
+    public GameObject trash2;
+    public GameObject trash3;
+
+    public void CollectTrash1()
+    {
+        trash1.SetActive(false);
+        GlobalData.KoinChange += TrashPrice;
+    }
+
+    public void CollectTrash2()
+    {
+        trash2.SetActive(false);
+        GlobalData.KoinChange += TrashPrice;
+    }
+
+    public void CollectTrash3()
+    {
+        trash3.SetActive(false);
+        GlobalData.KoinChange += TrashPrice;
+    }
+
     public void GoBackToTown()
     {
         SceneManager.LoadScene("MainScene");
