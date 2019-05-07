2e8981bd471c4890ee8ff185a3857e161971d0bf
MonikaLin <monika.linkaite@ktu.edu>
2019-05-07 22:36:53 +0300

    d
    d

---
+++ KoolKoalasCity/Assets/Controllers/DragAndDrop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour {
+
+    private bool selected;
+
+    void Update()
+    {
+        if (selected == true)
+        {
+            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(cursorPos.x, cursorPos.y);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            selected = false;
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            selected = true;
+        }
+    }
+}
