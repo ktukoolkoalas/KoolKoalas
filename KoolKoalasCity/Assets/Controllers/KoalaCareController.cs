11d69d29b8305095a0ed973ebd31de6466a825aa
Ieva-0 <lukoseviciute.ie@gmail.com>
2019-05-15 03:01:23 +0300

    hygiene bar, new particles, choice panel in restaurant

--- KoolKoalasCity/Assets/Controllers/KoalaCareController.cs
+++ KoolKoalasCity/Assets/Controllers/KoalaCareController.cs
@@ -5,25 +5,35 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Globalization;
+using System;
 
 public class KoalaCareController : MonoBehaviour
 {
-    private int _food;
-    private int _happiness;
+    //private int _food;
+    //private int _happiness;
+    //private int _clean;
 
     public Slider foodSlider;
     public Slider happinessSlider;
+    public Slider hygieneSlider;
 
     private bool _serverTime;
 
     public ParticleSystem hearts;
+    public ParticleSystem food;
+    public ParticleSystem bubbles;
     public GameObject sad;
     public GameObject happy;
+    public GameObject regeyes;
+    public GameObject happyeyes;
 
     public GameObject foodAlert;
     public GameObject playAlert;
+    public GameObject cleanAlert;
     public Button foodButton;
     public Button playButton;
+    public Button cleanButton;
 
     // Start is called before the first frame update
     void Start()
@@ -34,14 +44,14 @@ public class KoalaCareController : MonoBehaviour
         playAlert.SetActive(false);
         foodSlider.interactable = false;
         happinessSlider.interactable = false;
-        //updateStatus();
+        updateStatus();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(_food < 50 || _happiness < 50)
+        if(GlobalData.food < 39 || GlobalData.happy < 39 || GlobalData.clean < 39)
         {
             sad.SetActive(true);
             happy.SetActive(false);
@@ -90,6 +100,10 @@ public class KoalaCareController : MonoBehaviour
                     {
                         Play();
                     }
+                    if (objectToDrag.name == "clean")
+                    {
+                        Clean();
+                    }
                 }
                 objectToDrag.position = originalPosition;
                 objectToDragImage.raycastTarget = true;
@@ -102,7 +116,7 @@ public class KoalaCareController : MonoBehaviour
 
     void updateStatus()
     {
-        if (!PlayerPrefs.HasKey("_food"))
+        /*if (!PlayerPrefs.HasKey("_food"))
         {
             _food = 100;
             PlayerPrefs.SetInt("_food", _food);
@@ -120,44 +134,131 @@ public class KoalaCareController : MonoBehaviour
         else
         {
             _food = PlayerPrefs.GetInt("_happiness");
+        }*/
+        List<int> values = new List<int>();
+        values.Add(GlobalData.food);
+        values.Add(GlobalData.happy);
+        values.Add(GlobalData.clean);
+        int lowest = values[0];
+        int t = -1;
+        for(int i = 0; i < 2; i++)
+        {
+            if (values[i] < lowest)
+            {
+                lowest = values[i];
+                t = i;
+            }
         }
+        if(lowest > 39)
+        {
+            int timeElapsed;
+            switch(t)
+            {
+                case 0:
+                    timeElapsed = (DateTime.Now - GlobalData.lastFed).Seconds;
+                    break;
+                case 1:
+                    timeElapsed = (DateTime.Now - GlobalData.lastPlayed).Seconds;
+                    break;
+                case 2:
+                    timeElapsed = (DateTime.Now - GlobalData.lastCleaned).Seconds;
+                    break;
+                default:
+                    timeElapsed = 0;
+                    break;
+            }
+            int koins = lowest - 39;
+            Debug.Log(koins);
+            Debug.Log(timeElapsed);
+            if (timeElapsed < koins)
+            {
+                GlobalData.KoinChange = timeElapsed;
+            }
+            else GlobalData.KoinChange = koins;
+        }
+        GlobalData.food -= (DateTime.Now - GlobalData.lastFed).Seconds;
+        foodSlider.value = GlobalData.food;
+        GlobalData.happy -= (DateTime.Now - GlobalData.lastPlayed).Seconds;
+        happinessSlider.value = GlobalData.happy;
+        GlobalData.clean -= (DateTime.Now - GlobalData.lastCleaned).Seconds;
+        hygieneSlider.value = GlobalData.clean;
+        if (GlobalData.food < 0)
+            GlobalData.food = 0;
+        if (GlobalData.happy < 0)
+            GlobalData.happy = 0;
+        if (GlobalData.clean < 0)
+            GlobalData.clean = 0;
     }
     public void Feed()
     {
-        if (_food < 100)
+        if (GlobalData.food < 100)
         {
-            if (_food <= 80)
+            if (GlobalData.food <= 80)
             {
-                _food += 20;
+                GlobalData.food += 20;
             }
-            else _food = 100;
+            else GlobalData.food = 100;
             StartCoroutine(FillBar(foodSlider, 20));
-            StartCoroutine(EmitHearts());
+            StartCoroutine(EmitLeaves());
+            //StartCoroutine(LookHappy());
+            GlobalData.lastFed = DateTime.Now;
+            Debug.Log(GlobalData.food);
+
         }
         else
         {
             foodAlert.SetActive(true);
             playButton.interactable = false;
             foodButton.interactable = false;
+            cleanButton.interactable = false;
+        }
+    }
+    public void Clean()
+    {
+        if (GlobalData.clean < 100)
+        {
+            if (GlobalData.clean <= 80)
+            {
+                GlobalData.clean += 20;
+            }
+            else GlobalData.clean = 100;
+            StartCoroutine(FillBar(hygieneSlider, 20));
+            StartCoroutine(EmitBubbles());
+            //StartCoroutine(LookHappy());
+            GlobalData.lastCleaned = DateTime.Now;
+            Debug.Log(GlobalData.clean);
+
+
+        }
+        else
+        {
+            cleanAlert.SetActive(true);
+            playButton.interactable = false;
+            foodButton.interactable = false;
+            cleanButton.interactable = false;
         }
     }
     public void Play()
     {
-        if (_happiness < 100)
+        if (GlobalData.happy < 100)
         {
-            if (_happiness <= 80)
+            if (GlobalData.happy <= 80)
             {
-                _happiness += 20;
+                GlobalData.happy += 20;
             }
-            else _happiness = 100;
+            else GlobalData.happy = 100;
             StartCoroutine(FillBar(happinessSlider, 20));
             StartCoroutine(EmitHearts());
+            //StartCoroutine(LookHappy());
+            GlobalData.lastPlayed = DateTime.Now;
+            Debug.Log(GlobalData.happy);
         }
         else 
         {
             playAlert.SetActive(true);
             playButton.interactable = false;
             foodButton.interactable = false;
+            cleanButton.interactable = false;
         }
     }
 
@@ -165,21 +266,55 @@ public class KoalaCareController : MonoBehaviour
     {
         for(int i = 0; i < amount; i++)
         {
-            slider.value += 0.01f;
+            slider.value += 1;
             yield return null;
         }
+    }
+    IEnumerator EmptyBar(Slider slider, int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            slider.value -= 0.01f;
+            yield return null;
+        }
+    }
+    IEnumerator LookHappy()
+    {
+        happyeyes.SetActive(true);
+        regeyes.SetActive(false);
+        sad.SetActive(false);
+        happy.SetActive(true);
+        yield return new WaitForSeconds(1.0f);
+        happyeyes.SetActive(false);
+        regeyes.SetActive(true);
+        sad.SetActive(true);
+        happy.SetActive(false);
     }
     IEnumerator EmitHearts()
     {
         hearts.Play();
+        yield return null;
+
+    }
+    IEnumerator EmitLeaves()
+    {
+        food.Play();
+        yield return null;
+    }
+    IEnumerator EmitBubbles()
+    {
+        bubbles.Play();
         yield return null;
     }
     public void CloseAlert()
     {
         foodAlert.SetActive(false);
         playAlert.SetActive(false);
+        cleanAlert.SetActive(false);
         playButton.interactable = true;
         foodButton.interactable = true;
+        cleanButton.interactable = true;
+
     }
 
     public void GoToTown()
